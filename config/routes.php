c478bdceaff4c60ca00dd02553d59e79ecf9b79d
mqtent <mqtent@gmail.com>
2018-01-31 08:51:01 +0700

    Simply REST API

--- config/routes.php
+++ config/routes.php
@@ -49,13 +49,17 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
+    $routes->setExtensions(['json']);
+    $routes->resources('Products');
+
+
     $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
-
+    
     //User-Product
     $routes->connect('/user-product/:id', 
         ['controller' => 'Users', 'action' => 'getProductsByUserId'], 
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,34 +4,76 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Validation\Validator;
 // use Cake\View\View;
+use Cake\Controller\Component\RequestHandlerComponent;
 
 class ProductsController extends AppController
 {
 	public function initialize()
     {
         parent::initialize();
-        $this->loadComponent('Flash'); // Include the FlashComponent
+        $this->loadComponent('RequestHandler');
     }
+
     public function index()
     {
-        $this->set('curUserEmail', $this->Auth->user('email'));
-        $this->set('products', $this->Products->find('all'));
+        // Set the view vars that have to be serialized.
+        $this->set('products', $this->paginate());
+        // Specify which view vars JsonView should serialize.
+        $this->set('_serialize', ['products']);
     }
+
+    public function view($id)
+    {
+        $product = $this->Products->get($id);
+        $this->set([
+            'product' => $product,
+            '_serialize' => ['product']
+        ]);
+    }
+
     public function add()
     {
-    	$product = $this->Products->newEntity();
-    	if ($this->request->is('post')) //Check method POST
-    	{
-    		$product = $this->Products->patchEntity($product, $this->request->data);
-            $product->user_id = $this->Auth->user('id');
-            // dd($product);
-    		if ($this->Products->save($product)) {
-    			$this->Flash->success(__('Your product has been saved.'));
-    			return $this->redirect(['action' => 'index']);
-    		}
-    		$this->Flash->error(__('Unable to add your product.'));
-    	}
-    	$this->set('product', $product);
+        $recipe = $this->Recipes->newEntity($this->request->getData());
+        if ($this->Recipes->save($recipe)) {
+            $message = 'Saved';
+        } else {
+            $message = 'Error';
+        }
+        $this->set([
+            'message' => $message,
+            'recipe' => $recipe,
+            '_serialize' => ['message', 'recipe']
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $recipe = $this->Recipes->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            $recipe = $this->Recipes->patchEntity($recipe, $this->request->getData());
+            if ($this->Recipes->save($recipe)) {
+                $message = 'Saved';
+            } else {
+                $message = 'Error';
+            }
+        }
+        $this->set([
+            'message' => $message,
+            '_serialize' => ['message']
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $recipe = $this->Recipes->get($id);
+        $message = 'Deleted';
+        if (!$this->Recipes->delete($recipe)) {
+            $message = 'Error';
+        }
+        $this->set([
+            'message' => $message,
+            '_serialize' => ['message']
+        ]);
     }
 }
 ?>
