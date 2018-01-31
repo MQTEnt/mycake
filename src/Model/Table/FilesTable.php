bd9f98fefb002326617ce67ea88728d3d6c21346
mqtent <mqtent@gmail.com>
2018-01-31 15:39:20 +0700

    addBehavior note

--- src/Model/Table/FilesTable.php
+++ src/Model/Table/FilesTable.php
@@ -6,6 +6,6 @@ use Cake\ORM\Table;
 class FilesTable extends Table
 {
     public function initialize(array $config){
-        $this->addBehavior('Timestamp');
+        $this->addBehavior('Timestamp'); //Auto update 'created' and 'modified' field
     }
 }
